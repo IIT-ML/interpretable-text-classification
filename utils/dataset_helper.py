d5cc133f9862533b212bbe9d000c8cac8d41716f
Anneke <ahidayat@hawk.iit.edu>
2019-06-23 18:04:28 -0500

    Add dataset detail

--- utils/dataset_helper.py
+++ utils/dataset_helper.py
@@ -278,6 +278,151 @@ def load_peer_read(abs_path,
         meta['cl'], data['cl'], label['cl'] = get_parsed(path['cl'], data_mode, label_mode)
         meta['lg'], data['lg'], label['lg'] = get_parsed(path['lg'], data_mode, label_mode)
 
+        X_train_corpus, y_train = get_data(meta, data, label, data_mode, label_mode, 'train')
+        logging.info('Training data loaded.')
+        X_test_corpus, y_test = get_data(meta, data, label, data_mode, label_mode, 'test')
+        logging.info('Testing data loaded.')
+
+        y_train = y_train.astype(np.float32)
+        y_test = y_test.astype(np.float32)
+
+        if shuffle:
+            np.random.seed(random_state)
+            indices = np.random.permutation(len(y_train))       
+
+            X_train_corpus = [X_train_corpus[i] for i in indices]
+            y_train = y_train[indices]
+
+            indices = np.random.permutation(len(y_test))
+
+            X_test_corpus = [X_test_corpus[i] for i in indices]
+            y_test = y_test[indices]
+            logging.info('Shuffled.')
+
+        if lower:
+            X_train_corpus = [text.lower() for text in X_train_corpus]
+            X_test_corpus = [text.lower() for text in X_test_corpus]
+            logging.info('Lowered.')
+
+        if tokenize:
+            X_train_corpus = [word_tokenize(text) for text in X_train_corpus]
+            X_test_corpus = [word_tokenize(text) for text in X_test_corpus]
+            logging.info('Tokenized.')
+
+        return X_train_corpus, X_test_corpus, y_train, y_test
+    else:
+        raise ValueError('Path doesn\'t exists...')
+
+def load_arxiv(abs_path,
+                   tokenize=True, 
+                   lower=True, 
+                   shuffle=True, 
+                   random_state=42):
+    """
+    Load data for peerRead
+    
+    # Arguments
+        tokenize: Boolean
+        lower: Boolean
+        shuffle: Boolean
+        random_state: Integer
+    # Returns
+        X_train, X_test, y_train, y_test
+    
+    """
+    
+    import json
+    
+    def get_parsed(path, 
+                   data_mode, 
+                   label_mode):
+        """
+        get the file names under different conference folder
+        
+        # Arguments
+            path: Path to the parent folder
+            data_mode: dict.
+                See below on data_mode (key,value) -> ('train', 'path to train')
+            label_mode: dict.
+                same as data. But on label to get reviews
+        
+        """
+        parsed_data = {}
+        parsed_label = {}
+
+        for key,item in data_mode.items():
+            parsed_data[key] = sorted(glob.glob1(os.path.join(path, item),
+                                         '*'))
+        
+        for key,item in label_mode.items():
+            parsed_label[key] = sorted(glob.glob1(os.path.join(path, item),
+                                          '*'))
+            
+        meta = [(key, len(item)) for key,item in parsed_data.items()]
+
+        return meta, parsed_data, parsed_label
+    
+    def get_data(meta, data, label, data_mode, label_mode, mode=None):
+        """
+        
+        # Args
+            mode: {'train', 'test', 'dev'}
+        # returns
+            x, y
+        
+        """
+#         ignore = ['acl', 'iclr', 'coNLL']
+        if mode is not None:
+            x = []
+            y = []
+            for k in data.keys():
+                for i in range(len(data[k][mode])):
+                        # data
+                        with open(os.path.join(path[k], data_mode[mode], data[k][mode][i])) as json_file:  
+                            file = json.load(json_file) 
+                            if file['metadata']['sections'] is not None:
+                                temp = ' '
+                                for key in file['metadata']['sections']:
+                                    if key['heading'] is not None:
+                                        temp = ' '.join([temp, '{} {}'.format(key['heading'], key['text'].strip())])
+                        x.append(temp.replace('\n', ' '))
+
+#                         if k not in ignore:
+                        if k == 'ai':
+                            y.append(1)
+                        elif k == 'cl':
+                            y.append(2)
+                        elif k == 'lg':
+                            y.append(3)
+                assert len(x) == len(y), "data and label don\'t match."  
+            return x, np.array(y)
+        else:
+            raise ValueError('mode doesn\'t exists')
+
+    
+    if os.path.exists(abs_path): 
+        logging.info('Loading start...')
+        path = {}
+#         path['acl'] = os.path.join(abs_path,'data/acl_2017')
+#         path['iclr'] = os.path.join(abs_path,'data/iclr_2017')
+#         path['coNLL'] = os.path.join(abs_path, 'data/conll_2016')
+        path['ai'] = os.path.join(abs_path, 'data/arxiv.cs.ai_2007-2017')
+        path['cl'] = os.path.join(abs_path, 'data/arxiv.cs.cl_2007-2017')
+        path['lg'] = os.path.join(abs_path, 'data/arxiv.cs.lg_2007-2017')
+        data_mode = {'train' : 'train/parsed_pdfs/',
+                 'test' : 'test/parsed_pdfs/',
+                 'dev' : 'dev/parsed_pdfs/'}
+        label_mode = {'train' : 'train/reviews/',
+                 'test' : 'test/reviews/',
+                 'dev' : 'dev/reviews/'}
+
+        meta, data, label = {}, {}, {}
+#         meta['acl'], data['acl'], label['acl'] = get_parsed(path['acl'], data_mode, label_mode)
+#         meta['iclr'], data['iclr'], label['iclr'] = get_parsed(path['iclr'], data_mode, label_mode)
+#         meta['coNLL'], data['coNLL'], label['coNLL'] = get_parsed(path['coNLL'], data_mode, label_mode)
+        meta['ai'], data['ai'], label['ai'] = get_parsed(path['ai'], data_mode, label_mode)
+        meta['cl'], data['cl'], label['cl'] = get_parsed(path['cl'], data_mode, label_mode)
+        meta['lg'], data['lg'], label['lg'] = get_parsed(path['lg'], data_mode, label_mode)
 
         X_train_corpus, y_train = get_data(meta, data, label, data_mode, label_mode, 'train')
         logging.info('Training data loaded.')
@@ -314,6 +459,7 @@ def load_peer_read(abs_path,
     else:
         raise ValueError('Path doesn\'t exists...')
 
+        
 # TODO: load_amazon
 def load_amazon(path, shuffle=True, 
                 random_state=42, 
